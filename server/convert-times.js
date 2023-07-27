5633d095c0c8af746f48863e5f68a0769d97b848
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-07-27 12:12:45 +0200

    bugfix regex to decide if input is timestamp or date string; refactor decision logic to function getResponse

--- server/convert-times.js
+++ server/convert-times.js
@@ -1,38 +1,61 @@
-const errorJSON = { errorJSON: "Invalid Date" };
+const errorJSON = { error: "Invalid Date" };
 
 /* timestamp to utc date time string */
-export function getUTC(str) {
-    const num = Number(str);
-    if (isNaN(num)) {
-        return errorJSON;
+export function getUTC(timestamp) {
+    const number = Number(timestamp);
+    if (isNaN(number)) {
+        return [false, undefined, undefined];
     }
 
-    const date = new Date(num);
+    const date = new Date(number);
 
     if (date === null) {
-        return errorJSON;
+        return [false, undefined, undefined];
     }
 
-    const utc = date.toUTCString();
-    return utc;
+    const dateString = date.toUTCString();
+    return [true, dateString, number];
 }
 
 /* date time string to timestamp */
-export function getTimestamp(str) {
-    const date = new Date(str);
+export function getTimestamp(dateString) {
+    const date = new Date(dateString);
+    // console.log({ date });
     if (date === null) {
-        return errorJSON;
+        return [false, undefined, undefined];
     }
 
     const timestampStr = date.getTime();
-    const timestamp = Number(timestampStr);
+    const number = Number(timestampStr);
 
-    return timestamp;
+    return [true, number, dateString];
 }
 
 // export default { getTimestamp, getUTC };
 
 /* construct response json */
-export function makeJSON(timestamp, utc) {
-    return { unix: timestamp, utc: utc };
+export function makeJSON(timestampNumber, timestampStr) {
+    return { unix: timestampNumber, utc: timestampStr };
+}
+
+/* decide if input str
+is timestamp or date time string
+and return json accordingly */
+export function getResponse(str) {
+    if (/^\d+$/.test(str) === true) {
+        /* may be a timestamp */
+
+        // console.log("--- input is timestamp");
+
+        const [ok, dateString, timestampNumber] = getUTC(str);
+        const json = ok ? makeJSON(timestampNumber, dateString) : errorJSON;
+        return json;
+    }
+
+    // console.log("--- input is date string");
+
+    /* may be valid date string */
+    const [ok, timestampNumber, dateString] = getTimestamp(str);
+    const json = ok ? makeJSON(timestampNumber, dateString) : errorJSON;
+    return json;
 }
--- server/server.js
+++ server/server.js
@@ -1,29 +1,24 @@
 import express from "express";
-import { getTimestamp, getUTC, makeJSON } from "./convert-times.js";
+import { getTimestamp, getUTC, getResponse } from "./convert-times.js";
 
 const app = express();
 
 app.get("/api/:date", function (req, res) {
-    const obj = req.params;
+    const paramsObj = req.params;
+
     /* if path is missing date in /api/date */
-    if (obj.hasOwnProperty("date") === false) res.json(errorJSON);
-
-    /* can be either
-    number or
-    date time string */
-    const str = obj.date;
-
-    if (/\d+/.test(str) === true) {
-        /* its a timestamp */
-        const utc = getUTC(str);
-        const json = makeJSON(str, utc);
-        return res.json(json);
+    if (paramsObj.hasOwnProperty("date") === false) {
+        return errorJSON;
     }
 
-    /* may be valid date time string */
-    const timestamp = getTimestamp(str);
-    const json = makeJSON(timestamp, utc);
-    return res.json(json);
+    /* can be either
+        timestamp number or
+        date time string */
+
+    const str = paramsObj.date;
+
+    const response = getResponse(str);
+    res.json(response);
 });
 
 app.listen(3000, () => console.log("server is listening on 3000"));
